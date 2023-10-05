db3393e93d9b688e3293078314271f4344bfcdbf
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-10-05 18:00:20 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
