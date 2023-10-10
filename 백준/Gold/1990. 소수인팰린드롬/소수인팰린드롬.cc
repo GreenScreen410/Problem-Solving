3275e7a10f7cdd3f175d497f821675b06f455aa5
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-10-10 13:49:06 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 372 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.cc
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+bool palindrome(string s) {
+  string s1 = s;
+  string s2 = s1;
+  reverse(s2.begin(), s2.end());
+
+  if (s1 == s2) return true;
+  else return false;
+}
+
+bool prime(int n) {
+  if (n < 2)
+    return false;
+
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0)
+      return false;
+  }
+
+  return true;
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  for (int i = a; i <= 10000000; i++) {
+    if (i > b)
+      break;
+
+    if (palindrome(to_string(i)) && prime(i)) {
+      cout << i << "\n";
+    }
+  }
+
+  cout << "-1";
+
+  return 0;
+}
