dbb0ccedd24123a383e4b6d021664b61e920c72c
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-10-12 17:00:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N * 2; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
