4992deb723d5f606565a740921d4e75bfbe2a795
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-10-23 09:41:21 +0900

    [Bronze IV] Title: Любитель нулей, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27257. Любитель нулей/Любитель нулей.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string k;
+  cin >> k;
+
+  int answer = 0;
+  for (int i = 0; i < k.length(); i++) {
+    if (k[i] == '0') answer++;
+  }
+
+  for (int i = k.length() - 1; i != 0; i--) {
+    if (k[i] == '0') {
+      answer--;
+    } else {
+      break;
+    }
+  }
+
+  cout << answer;
+
+  return 0;
+}
