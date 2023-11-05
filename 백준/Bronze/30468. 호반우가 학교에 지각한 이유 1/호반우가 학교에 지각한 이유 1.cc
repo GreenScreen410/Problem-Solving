bbbb3a7f54afbea62310da08d960e62074cb671d
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-11-05 21:00:12 +0900

    [Bronze IV] Title: 호반우가 학교에 지각한 이유 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30468. 호반우가 학교에 지각한 이유 1/호반우가 학교에 지각한 이유 1.cc
+++ 백준/Bronze/30468. 호반우가 학교에 지각한 이유 1/호반우가 학교에 지각한 이유 1.cc
@@ -5,23 +5,11 @@ int main() {
   int STR, DEX, INT, LUK, N;
   cin >> STR >> DEX >> INT >> LUK >> N;
 
-  int count = 0;
-  if (STR + DEX + INT + LUK < N * 4) {
-    while (STR + DEX + INT + LUK < N * 4) {
-      if (STR <= DEX && STR <= INT && STR <= LUK) {
-        STR++;
-      } else if (DEX <= STR && DEX <= INT && DEX <= LUK) {
-        DEX++;
-      } else if (INT <= STR && INT <= DEX && INT <= LUK) {
-        INT++;
-      } else if (LUK <= STR && LUK <= DEX && LUK <= INT) {
-        LUK++;
-      }
-      count++;
-    }
+  if (STR + DEX + INT + LUK >= N * 4) {
+    cout << "0";
+  } else {
+    cout << N * 4 - (STR + DEX + INT + LUK);
   }
-
-  cout << count;
 
   return 0;
 }
