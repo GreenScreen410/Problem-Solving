e1d6e06ec9d52e5a3f99a37234c98f5412eb4057
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-11-05 21:26:35 +0900

    [Bronze III] Title: 분수좋아해?, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10474. 분수좋아해？/분수좋아해？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  while (1) {
+    int a, b;
+    cin >> a >> b;
+
+    if (a == 0 && b == 0) break;
+
+    cout << a / b << " " << a - (a / b) * b << " / " << b << "\n";
+  }
+
+  return 0;
+}
