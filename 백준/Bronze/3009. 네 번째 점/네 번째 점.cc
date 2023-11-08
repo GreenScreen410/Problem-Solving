39ac95575817f5d32c396c3e45f8bc7a461d4ae7
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-11-08 11:04:21 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int> x(4), y(4);
+  for (int i = 0; i < 3; i++) {
+    cin >> x[i] >> y[i];
+  }
+
+  if (x[0] == x[1]) cout << x[2] << " ";
+  else if (x[0] == x[2]) cout << x[1] << " ";
+  else cout << x[0] << " ";
+
+  if (y[0] == y[1]) cout << y[2];
+  else if (y[0] == y[2]) cout << y[1];
+  else cout << y[0];
+
+  return 0;
+}
