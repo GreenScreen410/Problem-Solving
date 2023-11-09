d58a059904d3278ce660cd7d951ad8cb97582773
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-11-09 09:40:46 +0900

    [Bronze III] Title: Sort 마스터 배지훈, Time: 244 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Bronze/17263. Sort 마스터 배지훈/Sort 마스터 배지훈.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+  sort(A.begin(), A.end());
+
+  cout << A[N - 1];
+
+  return 0;
+}
