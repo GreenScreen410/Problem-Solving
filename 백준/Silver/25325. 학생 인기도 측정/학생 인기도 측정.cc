d8b9363b810b199ded6f20da8c5b4b274c9f22a1
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2023-12-31 10:25:13 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool compare(const pair<string, int>& a, const pair<string, int>& b) {
+    if (a.second == b.second) return a.first < b.first;
+    return a.second > b.second;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  
+  vector<pair<string, int>> name(n);
+  for (int i = 0; i < n; i++) {
+    cin >> name[i].first;
+  }
+  
+  string A;
+  while (cin >> A) {
+    for (int i = 0; i < n; i++) {
+      if (name[i].first == A) name[i].second++;
+    }
+  }
+  sort(name.begin(), name.end(), compare);
+  
+  for (int i = 0; i < n; i++) {
+    cout << name[i].first << " " << name[i].second << "\n";
+  }
+  
+  return 0;
+}
