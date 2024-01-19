d210a0f2e05ab4e771030540bb4341ba27ec0b6a
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2024-01-19 09:21:55 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  for (int i = 0; i < N * 2; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    for (int j = 0; j < N * 5 - 2 * N; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    for (int j = 0; j < N * 5 - 2 * N; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < N; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N * 5; j++) {
+      cout << "@";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
