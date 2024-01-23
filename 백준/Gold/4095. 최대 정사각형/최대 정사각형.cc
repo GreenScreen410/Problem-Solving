343c694c1688ccf3929ad3b42221d6aca032b7d4
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2024-01-23 12:47:04 +0900

    [Gold IV] Title: 최대 정사각형, Time: 1400 ms, Memory: 6912 KB -BaekjoonHub

---
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  while (true) {
+    int N, M;
+    cin >> N >> M;
+    if (N == 0 && M == 0) break;
+  
+    vector<vector<char>> v(N + 1, vector<char>(M + 1));
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= M; j++) {
+        cin >> v[i][j];
+      }
+    }
+  
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1));
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= M; j++) {
+        if (v[i][j] == '1') {
+          dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+        }
+      }
+    }
+  
+    int length = 0;
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= M; j++) {
+        if (length < dp[i][j]) {
+          length = dp[i][j];
+        }
+      }
+    }
+  
+    cout << length << "\n";
+  }
+
+  return 0;
+}
