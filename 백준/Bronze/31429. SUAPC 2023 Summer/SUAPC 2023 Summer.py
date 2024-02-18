f220f772e51e128a3c71dd887b62ebfbe3b7d52f
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2024-02-19 08:45:47 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+if N == 1:
+    print(12, 1600)
+elif N == 2:
+    print(11, 894)
+elif N == 3:
+    print(11, 1327)
+elif N == 4:
+    print(10, 1311)
+elif N == 5:
+    print(9, 1004)
+elif N == 6:
+    print(9, 1178)
+elif N == 7:
+    print(9, 1357)
+elif N == 8:
+    print(8, 837)
+elif N == 9:
+    print(7, 1055)
+elif N == 10:
+    print(6, 556)
+elif N == 11:
+    print(6, 773)
