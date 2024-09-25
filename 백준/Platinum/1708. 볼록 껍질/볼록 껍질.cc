54735f9295d90132138cf85c719395c37a4c65d0
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2024-09-25 11:26:52 +0900

    [Platinum V] Title: 볼록 껍질, Time: 92 ms, Memory: 5156 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
@@ -11,38 +11,23 @@ long long ccw(Point a, Point b, Point c) {
   return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
 }
 
-bool compare_y(Point a, Point b) {
-  if (a.y == b.y) return a.x < b.x;
-  else return a.y < b.y;
-}
-
-bool compare_ccw(Point points, Point a, Point b) {
-  long long cp = ccw(points, a, b);
-  if (cp == 0) {
-    if (a.y == b.y) return a.x < b.x;
-    return a.y < b.y;
-  }
-  return cp > 0;
-}
-
 vector<Point> convex_hull(vector<Point> points) {
-  sort(points.begin(), points.end(), compare_y);
+  sort(points.begin(), points.end(), [](Point a, Point b) {
+    return a.y < b.y || (a.y == b.y && a.x < b.x);
+  });
   Point pivot = points[0];
   sort(points.begin() + 1, points.end(), [pivot](Point a, Point b) {
-    return compare_ccw(pivot, a, b);
+    long long cp = ccw(pivot, a, b);
+    return cp > 0 || (cp == 0 && (a.y < b.y || (a.y == b.y && a.x < b.x)));
   });
-
-  vector<Point> hull;
-  hull.push_back(points[0]);
-  hull.push_back(points[1]);
-
+  
+  vector<Point> hull = {points[0], points[1]};
   for (int i = 2; i < points.size(); i++) {
     while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), points[i]) <= 0) {
       hull.pop_back();
     }
     hull.push_back(points[i]);
   }
-
   return hull;
 }
 
