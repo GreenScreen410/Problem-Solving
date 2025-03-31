747e01fc1e5c099df38251bf0c559100eeba53ab
Mingyu Jung <pauljjang410@gmail.com>
2025-03-31 23:50:01 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -3,45 +3,40 @@ using namespace std;
 int parent[1000001];
 
 int find(int x) {
-	if (parent[x] == x) return x;
-	parent[x] = find(parent[x]);
-	return parent[x];
+    return parent[x] == x ? x : parent[x] = find(parent[x]);
 }
 void merge(int x, int y) {
-	x = find(x);
-	y = find(y);
-	if (x == y) return;
-	parent[y] = x;
+    x = find(x);
+    y = find(y);
+    if (x != y) parent[y] = x;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-	int n, m;
-	cin >> n >> m;
-	
-	for (int i = 1; i < 1000001; i++) {
-	    parent[i] = i;
-	}
-		
-	for (int i = 0; i < m; i++) {
-	    int q, a, b;
-	    cin >> q >> a >> b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-		if (q == 0) {
-			merge(a, b);
-		}
-		else {
-			if (find(a) == find(b)) {
-			    cout << "YES\n";
-			}
-			else {
-			    cout << "NO\n";
-			}
-		}
-	}
-	
-	return 0;
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i < 1000001; i++) {
+        parent[i] = i;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int q, a, b;
+        cin >> q >> a >> b;
+
+        if (q == 0) {
+            merge(a, b);
+        } else {
+            if (find(a) == find(b)) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
+            }
+        }
+    }
+
+    return 0;
 }
