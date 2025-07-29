a1329a06c7483bd890b3f359438c39b293ab47c6
Mingyu Jung <pauljjang410@gmail.com>
2025-07-29 10:47:39 +0900

    🎨 convex_hull: 코드 길이 개선

--- greenSTL/geometry/convex_hull.cpp
+++ greenSTL/geometry/convex_hull.cpp
@@ -1,25 +1,19 @@
 template <typename T>
 vector<Point<T>> convex_hull(vector<Point<T>> points) {
-  if (points.size() <= 2) return points;
-
-  constexpr long double EPS = 1e-9L;
   sort(points.begin(), points.end());
 
-  vector<Point<T>> lower;
-  for (auto& p : points) {
-    while (lower.size() >= 2 && static_cast<long double>(lower[lower.size() - 2].ccw(lower.back(), p)) <= EPS) lower.pop_back();
+  vector<Point<T>> lower, upper;
+  for (const auto& p : points) {
+    while (lower.size() >= 2 && static_cast<long double>(end(lower)[-2].ccw(lower.back(), p)) <= 1e-9L) lower.pop_back();
     lower.push_back(p);
   }
-
-  vector<Point<T>> upper;
-  for (auto it = points.rbegin(); it != points.rend(); it++) {
-    while (upper.size() >= 2 && static_cast<long double>(upper[upper.size() - 2].ccw(upper.back(), *it)) <= EPS) upper.pop_back();
-    upper.push_back(*it);
+  for (const auto& p : views::reverse(points)) {
+    while (upper.size() >= 2 && static_cast<long double>(end(upper)[-2].ccw(upper.back(), p)) <= 1e-9L) upper.pop_back();
+    upper.push_back(p);
   }
 
-  vector<Point<T>> hull;
-  hull.assign(lower.begin(), lower.end() - 1);
-  hull.insert(hull.end(), upper.begin(), upper.end() - 1);
+  lower.pop_back();
+  lower.insert(lower.end(), upper.begin(), upper.end() - 1);
 
-  return hull;
+  return lower;
 }
