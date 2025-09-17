c5a4f295099262b8841034110d7775c630e63ea0
Mingyu Jung <pauljjang410@gmail.com>
2025-09-17 15:38:42 +0900

    ✨ point: 다양한 함수 추가

--- greenSTL/geometry/point.cpp
+++ greenSTL/geometry/point.cpp
@@ -3,15 +3,15 @@ struct Point {
   T x, y;
   auto operator<=>(const Point<T>&) const = default;
 
-  T ccw(const Point<T>& b, const Point<T>& c) const {
-    return (b.x - x) * (c.y - y) - (b.y - y) * (c.x - x);
-  }
+  Point operator+(const Point& other) const { return {x + other.x, y + other.y}; }
+  Point operator-(const Point& other) const { return {x - other.x, y - other.y}; }
+  Point operator*(T scalar) const { return {x * scalar, y * scalar}; }
+  Point operator/(T scalar) const { return {x / scalar, y / scalar}; }
 
-  T dist2(const Point<T>& other) const {
-    return (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y);
-  }
-
-  long double dist(const Point<T>& other) const {
-    return sqrt(dist2(other));
-  }
+  T dot(const Point& other) const { return x * other.x + y * other.y; }
+  T cross(const Point& other) const { return x * other.y - y * other.x; }
+  T ccw(const Point<T>& b, const Point<T>& c) const { return (b - *this).cross(c - *this); }
+  T norm2() const { return x * x + y * y; }
+  T dist2(const Point<T>& other) const { return (*this - other).norm2(); }
+  long double dist(const Point<T>& other) const { return sqrt(dist2(other)); }
 };
