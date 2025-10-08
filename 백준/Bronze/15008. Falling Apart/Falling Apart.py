3f12b6ca7d4e0d9a7a16cbc0d5c2b461a08d4a53
Mingyu Jung <pauljjang410@gmail.com>
2025-10-09 00:15:43 +0900

    [Bronze I] Title: Falling Apart, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/15008. Falling Apart/Falling Apart.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort(reverse=True)
+
+alice = 0
+bob = 0
+for i in range(n):
+    if i % 2 == 0:
+        alice += a[i]
+    else:
+        bob += a[i]
+
+print(alice, bob)
