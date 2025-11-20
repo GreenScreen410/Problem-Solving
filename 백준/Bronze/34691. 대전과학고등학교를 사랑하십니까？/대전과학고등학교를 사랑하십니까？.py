ddeab04cf9008f71bc07da70d9dc94de19f058e1
Mingyu Jung <pauljjang410@gmail.com>
2025-11-20 13:05:56 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    s = input()
+    if s == "animal":
+        print("Panthera tigris")
+    elif s == "flower":
+        print("Forsythia koreana")
+    elif s == "tree":
+        print("Pinus densiflora")
+    elif s == "end":
+        break
