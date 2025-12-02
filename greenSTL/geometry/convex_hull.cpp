7cc42432a842ae82be7d8462216bede63cd4a750
GreenScreen410 <pauljjang410@gmail.com>
2025-12-03 02:46:31 +0900

    🎨 convex_hull: ranges::sort으로 변경

--- greenSTL/geometry/convex_hull.cpp
+++ greenSTL/geometry/convex_hull.cpp
@@ -1,14 +1,14 @@
 template <typename T>
 vector<Point<T>> convex_hull(vector<Point<T>> points) {
-  sort(points.begin(), points.end());
+  ranges::sort(points);
 
   vector<Point<T>> lower, upper;
   for (const auto& p : points) {
-    while (lower.size() >= 2 && static_cast<long double>(end(lower)[-2].ccw(lower.back(), p)) <= 1e-9L) lower.pop_back();
+    while (lower.size() >= 2 && end(lower)[-2].ccw(lower.back(), p) <= 0) lower.pop_back();
     lower.push_back(p);
   }
   for (const auto& p : views::reverse(points)) {
-    while (upper.size() >= 2 && static_cast<long double>(end(upper)[-2].ccw(upper.back(), p)) <= 1e-9L) upper.pop_back();
+    while (upper.size() >= 2 && end(upper)[-2].ccw(upper.back(), p) <= 0) upper.pop_back();
     upper.push_back(p);
   }
 
