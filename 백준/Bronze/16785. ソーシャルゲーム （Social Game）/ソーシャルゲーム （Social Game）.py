7bffd30af2464663fdf8aeedca6c7487f59bd441
Mingyu Jung <pauljjang410@gmail.com>
2026-02-14 00:55:51 +0900

    [Bronze III] Title: ソーシャルゲーム (Social Game), Time: 260 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/16785. ソーシャルゲーム （Social Game）/ソーシャルゲーム （Social Game）.py
+++ 백준/Bronze/16785. ソーシャルゲーム （Social Game）/ソーシャルゲーム （Social Game）.py
@@ -1,10 +1,11 @@
 A, B, C = map(int, input().split())
 
-total_coins_per_week = 7 * A + B
-full_weeks = C // total_coins_per_week
-additional_days = 0
+coins = 0
+days = 0
+while coins < C:
+    days += 1
+    coins += A
+    if days % 7 == 0:
+        coins += B
 
-if C % total_coins_per_week != 0:
-    additional_days = (C % total_coins_per_week + A - 1) // A
-
-print(full_weeks * 7 + additional_days)
+print(days)
